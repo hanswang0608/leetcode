c3301ad8987bf07b47155d829a797c748cb78f73
Hans Wang <hanswang0168@gmail.com>
2024-06-06 23:35:39 -0400

    update

--- Merge Sorted Array/main.cpp
+++ Merge Sorted Array/main.cpp
@@ -8,32 +8,24 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
-        vector<int> temp;
-        int i = 0;
-        int j = 0;
-        while (i < m && j < n)
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0)
         {
-            if (nums1[i] < nums2[j])
+            if (i >= 0 && nums1[i] > nums2[j])
             {
-                temp.push_back(nums1[i]);
-                i++;
+                nums1[k] = nums1[i];
+                i--;
             }
             else
             {
-                temp.push_back(nums2[j]);
-                j++;
+                nums1[k] = nums2[j];
+                j--;
             }
+            k--;
         }
-        if (i < m)
-        {
-            temp.insert(temp.end(), nums1.begin() + i, nums1.begin() + m);
-        }
-        if (j < n)
-        {
-            temp.insert(temp.end(), nums2.begin() + j, nums2.end());
-        }
-
-        nums1 = temp;
     }
 };
 
